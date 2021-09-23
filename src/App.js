2d480b8c404fa1ee90270ac96bff9aba8caafd5a
mark <markpackham1@gmail.com>
2021-09-23 12:26:11 +0100

    style the header
    https://youtu.be/02zO0hZmwnw?t=1859

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Header />
       <Container>
         <h1>Test</h1>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
